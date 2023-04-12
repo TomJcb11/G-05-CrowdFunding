24180e1b224aa8b8d2511e77882dcf3476f08051
K I <113931682+TomJcb11@users.noreply.github.com>
2023-04-12 15:32:38 +0200

    f On branch tom

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use(express.static("/Users/alex/Documents/GitHub/G-05-CrowdFunding/frontend/dist/frontend"));
+app.use(express.static("../frontend/dist/frontend"));
 
 app.get('/', (req, res) => {
-    res.sendFile("/Users/alex/Documents/GitHub/G-05-CrowdFunding/frontend/dist/frontend/index.html");
+    res.sendFile("../frontend/dist/frontend/src/index.html");
 });
 
 app.listen(port, () => {
