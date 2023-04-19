6325553957c4c0a00cde4dacb6565ae0ef6a5620
AlexApostolidis <HE202156@students.ephec.be>
2023-04-20 01:26:28 +0200

    ajout des api "/api/users" et "/api/users/:id", les requêtes get sont faites dans le fichiers controllers_get.js et 2 routes on été donc ajouté a routes.js

--- backend/controllers/controllers_get.js
+++ backend/controllers/controllers_get.js
@@ -1,5 +1,38 @@
+const {Client} = require('pg');
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'giverr'
+});
+
+//test connection à la base de données
+client.connect(console.log('connexion réussie'));
+
+// récupération de tous les utilisateurs
+const getAllUsers = (req, res) => {
+
+    client.query('SELECT * FROM utilisateurs', (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.status(200).json(result.rows);
+    });
+};
+
+const getOneUser = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    client.query('SELECT * FROM utilisateurs WHERE id_utilisateurs = $1', [id], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.status(200).json(result.rows);
+    });
+};
 
 
 // exportation des fonctions
 module.exports = {
+    getAllUsers,
+    getOneUser
 };
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-//const get = require('../controllers/controllers_get');
+const get = require('../controllers/controllers_get');
 //const post = require('../controllers/controllers_post');
 //const put = require('../controllers/controler_put');
 //const del = require('../controllers/controler_delete');
 
+
 router.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+router.get('/api/users', get.getAllUsers);
+
+router.get('/api/users/:id', get.getOneUser);
 
 // exportation des routes
 module.exports = router;
