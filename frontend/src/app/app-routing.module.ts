480f3aec3d83ef907c83dd7528bc9ee21e9ffae3
AlexApostolidis <HE202156@students.ephec.be>
2023-04-24 12:45:09 +0200

    Ajout du routage des pages angular avec le module @angular/router

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: Component }
+  ];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
