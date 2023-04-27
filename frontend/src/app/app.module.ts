dd9b18121466a0660f13c25042c0045537466e09
K I <113931682+TomJcb11@users.noreply.github.com>
2023-04-27 02:44:36 +0200

    recherche sur data sans passer par l'api pour le moment

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -21,7 +22,8 @@ import { ProjectHudComponent } from './project-hud/project-hud.component';
     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/project-hud/project-hud.component.ts
+++ frontend/src/app/project-hud/project-hud.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-project-hud',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-hud.component.scss']
 })
 export class ProjectHudComponent {
+  project: any;
 
+
+
+
+
+constructor(private http: HttpClient) {}
+
+ngOnInit() {
+  const projectId = '1'; // ID du projet que vous voulez afficher
+
+  this.http.get(`https://votre-api.com/projets/${projectId}`).subscribe((data) => {
+    this.project = data;
+  });
+}
 }
