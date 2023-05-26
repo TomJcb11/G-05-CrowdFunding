0711d85ca03c1c03022c79e6aa0b469840b2b36e
K I <113931682+TomJcb11@users.noreply.github.com>
2023-05-26 15:49:10 +0200

    retrait du project component dans app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,17 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
-
+import { RouterModule } from '@angular/router'; // Importer RouterModule ici
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './navbar/home/home.component';
 import { MyProjectComponent } from './navbar/my-project/my-project.component';
 import { DropdownComponent } from './navbar/dropdown/dropdown.component';
-import { ProjetsComponent } from './projets/projets.component';
 import { AllTheProjectsComponent } from './projets/all-the-projects/all-the-projects.component';
 import { OneProjectBasedOnIdComponent } from './projets/one-project-based-on-id/one-project-based-on-id.component';
 import { OddProjetComponent } from './projets/odd-projet/odd-projet.component';
@@ -21,9 +18,6 @@ import { SettingsComponent } from './navbar/dropdown/settings/settings.component
 import { LoginComponent } from './navbar/dropdown/login/login.component';
 import { DonationsComponent } from './projets/donations/donations.component';
 
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,26 +25,20 @@ import { DonationsComponent } from './projets/donations/donations.component';
     HomeComponent,
     MyProjectComponent,
     DropdownComponent,
-    ProjetsComponent,
     AllTheProjectsComponent,
     OneProjectBasedOnIdComponent,
     OddProjetComponent,
     ProfileComponent,
     SettingsComponent,
     LoginComponent,
-    DonationsComponent,
-
-
-
-
+    DonationsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    FormsModule
-
-
+    FormsModule,
+    RouterModule // Ajouter RouterModule ici
   ],
   providers: [],
   bootstrap: [AppComponent]
