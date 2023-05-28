3a745c7435929bfc9243cf48d72b3351a863badd
K I <113931682+TomJcb11@users.noreply.github.com>
2023-05-28 14:25:35 +0200

    try to fix conflict

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 
@@ -23,25 +22,18 @@ import { LoginComponent } from './navbar/dropdown/login/login.component';
 import { DonationComponent } from './donation/donation.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PopupComponent } from './popup/popup.component';
-import { RouterModule } from '@angular/router';
-
-
-=======
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './navbar/home/home.component';
-import { SearchComponent } from './navbar/search/search.component';
+
 import { MyProjectBComponent } from './navbar/my-project-b/my-project-b.component';
-import { DropdownComponent } from './navbar/dropdown/dropdown.component';
+
 import { MyProjectsComponent } from './my-projects/my-projects.component';
 import { CustomConfirmDialogComponent } from './custom-confirm-dialog/custom-confirm-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MyProjectPageComponent } from './my-project-page/my-project-page.component';
 import { ModifyProjectComponent } from './modify-project/modify-project.component';
-import { FormsModule } from '@angular/forms';
+
 import { ModifyProjectPageComponent } from './modify-project-page/modify-project-page.component';
 
 
@@ -50,14 +42,12 @@ const appRoutes: Routes = [
   { path: 'my-project', component: MyProjectPageComponent },
   { path: 'my-project/modify/:id', component: ModifyProjectPageComponent}
 ];
->>>>>>> a88269382279df5fc5803de048dd8eea92ebafa2
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-<<<<<<< HEAD
     MyProjectComponent,
     DropdownComponent,
     ProjetsComponent,
@@ -68,38 +58,25 @@ const appRoutes: Routes = [
     SettingsComponent,
     LoginComponent,
     DonationComponent,
-    PopupComponent
-
-
-
-=======
-    SearchComponent,
+    PopupComponent,
     MyProjectBComponent,
-    DropdownComponent,
     MyProjectsComponent,
     CustomConfirmDialogComponent,
     MyProjectPageComponent,
     ModifyProjectComponent,
     ModifyProjectPageComponent,
-    
->>>>>>> a88269382279df5fc5803de048dd8eea92ebafa2
+
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-<<<<<<< HEAD
-    FormsModule,
-    BrowserAnimationsModule,
     RouterModule,
     AppRoutingModule,
-
-=======
     BrowserAnimationsModule,
     MatDialogModule,
     MatButtonModule,
     FormsModule,
     RouterModule.forRoot(appRoutes)
->>>>>>> a88269382279df5fc5803de048dd8eea92ebafa2
   ],
   exports:[RouterModule],
   providers: [],
